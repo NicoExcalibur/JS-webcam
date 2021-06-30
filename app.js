773c9e6ae76cbacf1c8b6a4abe0653a6515fe325
NicoExcalibur <nico.zanz@gmail.com>
2021-06-30 11:01:45 +0200

    fixed problem with stream display & delete json file

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-  
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
 
 function getVideo() {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    .then(localMediaStream => {
-        console.log(localMediaStream);
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+      // video.src = window.URL.createObjectURL(localMediaStream); /!\ deprecated way
+      try {
+        video.srcObject = localMediaStream;
+      } catch (error) {
         video.src = window.URL.createObjectURL(localMediaStream);
-        video.play();
+      }
+      video.play();
     })
+    .catch((err) => {
+      console.error("Oh noooo !", err);
+    });
 }
 
-// getVideo();
+getVideo();
